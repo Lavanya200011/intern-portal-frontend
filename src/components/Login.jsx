918a7996e036a9e2d35d14f404492a9b25c3894c
lavanya <lavanyathawkar@gmail.com>
2025-09-03 21:06:44 +0530

    server api url added (login)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,7 +11,7 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   try {
-    const res = await fetch("http://localhost:5000/api/login", {
+    const res = await fetch("https://intern-portal-backend-yin4.onrender.com/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
